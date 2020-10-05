a661460ecd0428ad6422b8fb1dad0ae4f150040d
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-10-05 09:50:39 +0700

    Initial commit

---
+++ src/app.kt
@@ -0,0 +1,28 @@
+fun main(args: Array<String>){
+    print("Введите текст:")
+    val Str1: String = readLine().toString()
+
+    //region 1 задание
+    println("Задание1:")
+    println(Str1)
+    //endregion
+    //region 2 задание
+    println("Задание 2:")
+    var ListSort = Str1.split(" ").sorted()
+    for (i in ListSort){
+        println(i)
+    }
+    //endregion
+    //region 3 задание
+    println("Задание 3:")
+    //endregion
+    //region 4 задание
+    println("Задание 4:")
+    //endregion
+    //region 5 задание
+    println("Задание 5:")
+    //endregion
+}
+fun MyFun1(){
+
+}

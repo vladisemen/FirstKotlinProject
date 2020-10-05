4a9e8d55df77c04a97c9f53834aa2c469972d2ed
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-10-05 14:46:30 +0700

    05.10.20

--- src/app.kt
+++ src/app.kt
@@ -1,28 +1,64 @@
 fun main(args: Array<String>){
-    print("Введите текст:")
-    val Str1: String = readLine().toString()
+    var List: Array<String>
+    //region 6 задание
+    if(args.size==0){
+        val Str1: String = readLine().toString()
+        List = Str1.split(" ").toTypedArray()
+    }else{
+        List=args
+    }
+    //endregion
 
     //region 1 задание
-    println("Задание1:")
-    println(Str1)
+    println("Задание 1:")
+    for (i in List){
+        println(i)
+    }
     //endregion
     //region 2 задание
     println("Задание 2:")
-    var ListSort = Str1.split(" ").sorted()
+    var ListSort = List.sorted()
     for (i in ListSort){
         println(i)
     }
     //endregion
     //region 3 задание
     println("Задание 3:")
+    val ListUnique : MutableList<String> = mutableListOf()
+    var Check1: Boolean = true
+    for(element in ListSort){
+        Check1 = true
+        Check1 = !ListUnique.contains(element)
+        if (Check1){
+            ListUnique.add(element)
+        }
+    }
+    for (i in ListUnique){
+        println(i)
+    }
     //endregion
     //region 4 задание
     println("Задание 4:")
+    var MapCollection: MutableMap<String, Int> = mutableMapOf()
+    var CountRepeat: Int = 0
+    for(element in ListUnique){
+        CountRepeat = 0
+        for (element1 in ListSort){
+            if (element == element1){
+                CountRepeat++
+            }
+        }
+        MapCollection.put(element, CountRepeat)
+    }
+    for (i in MapCollection){
+        println(i.key+" "+ i.value)
+    }
     //endregion
     //region 5 задание
+    val result = MapCollection.toList().sortedByDescending { (_, value) -> value}.toMap()
     println("Задание 5:")
+    for (i in result){
+        println(i.key+" "+ i.value)
+    }
     //endregion
-}
-fun MyFun1(){
-
 }

c362fb3f7811490fe83bcda587b880531b417aa2
vladisemen <40513696+vladisemen@users.noreply.github.com>
2020-10-16 18:10:09 +0700

    #2  Задание 2
    Убрал regions, из-за ненадобности, так как код получился компактным и аккуртаным

--- src/app.kt
+++ src/app.kt
@@ -1,27 +1,25 @@
 fun main(args: Array<String>) {
-    //region 6 задание
     val list: Array<String> = if (args.isEmpty()) // если через параметр ничего не пришло, читаем поток stdin
     {
         readLine().toString().split(" ").toTypedArray()
     } else {
         args
     }
-    //endregion
-    //region 1 задание
+
     println("Задание 1:")
     list.forEach(::println)
-    //endregion
-    //region 2 задание
+
     println("Задание 2:")
     list.sorted().forEach(::println)
+
     println("Задание 3:")
     list.sorted().toSet().forEach(::println)
 
     println("Задание 4:")
     val mapCollection = list.sorted().groupingBy { it }.eachCount()
     printCollection(mapCollection)
+
     println("Задание 5:")
-    //endregion
     val result = mapCollection.toList().sortedByDescending { (_, value) -> value }.toMap()
     printCollection(result)
 }

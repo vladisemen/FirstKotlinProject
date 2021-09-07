92be9c4f282471fe87ee481649ac7915fc5d4ff3
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-09-07 13:32:45 +0700

    Create task6.kt
    did task 6

---
+++ src/task6.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+    val list: Array<String> = if (args.isEmpty())
+    {
+        readLine().toString().split(" ").toTypedArray() //read stdin
+    } else {
+        args
+    }
+
+    println("requirement 1:")
+    list.forEach(::println)
+
+    println("requirement 2:")
+    list.sorted().forEach(::println)
+
+    println("requirement 3:")
+    list.sorted().toSet().forEach(::println)
+
+    println("requirement 4:")
+    val mapCollection = list.sorted().groupingBy { it }.eachCount()
+    printCollection(mapCollection)
+
+    println("requirement 5:")
+    val result = mapCollection.toList().sortedByDescending { (_, value) -> value }.toMap()
+    printCollection(result)
+}

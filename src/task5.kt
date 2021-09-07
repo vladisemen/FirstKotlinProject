9e84b014afcfd3f0041292d9a2d80379936f3118
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-09-07 13:29:34 +0700

    Create task5.kt
    did task 5

---
+++ src/task5.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+    val list: Array<String> = args
+
+    println("requirement 1:")
+    list.forEach(::println)
+
+    println("requirement 2:")
+    list.sorted().forEach(::println)
+
+    println("requirement 3:")
+    list.sorted().toSet().forEach(::println)
+
+    println("requirement 4:")
+    val mapCollection = list.sorted().groupingBy { it }.eachCount()
+    printCollection(mapCollection)
+
+    println("requirement 5:")
+    val result = mapCollection.toList().sortedByDescending { (_, value) -> value }.toMap()
+    printCollection(result)
+}
+

c06ecdcbf84262a41791ca1c2b9ca04f77269935
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-09-08 11:49:46 +0700

    Update TEST.bat
    tests

--- TEST.bat
+++ TEST.bat
@@ -1,42 +1,31 @@
 @echo off
 set /a num=0
-  @echo 1 тест- ввод значений через параметр командной строки
-java -jar app.jar the quick brown fox jumps over the lazy dog > out.txt
-fc /a out.txt true_out.txt
+set TestDate=the quick brown fox jumps over the lazy dog
+for /l %%i in (1,1,5) do (
+	echo %%i test
+	java -jar jar/task%%i.jar %TestDate% > out.txt
+	fc /a out.txt true_out/task%%i.txt
+	if ERRORLEVEL 1 (
+	  echo !Error: %%i test failed!
+	) else (
+	  echo %%i test passed
+	  set /a num+=1
+	)
+	  echo ----------------------
+)
+
+echo 6 test- input values throught stdin
+echo %TestDate%| java -jar jar/task6.jar > out.txt
+
+fc /a out.txt true_out/task5.txt
 if ERRORLEVEL 1 (
-  echo Ошибка: 1 тест не пройден
-    echo ----------------------
+  echo !Error: 6 test failed!
 ) else (
-  echo 1 тест успешно пройден
+  echo 6 test passed
+  set /a num+=1
+)
   echo ----------------------
-  set /a num+=1
-)
 
-  @echo 2 тест- ввод значений через поток stdin
-echo the quick brown fox jumps over the lazy dog| java -jar app.jar > out.txt
-fc /a out.txt true_out.txt
-if ERRORLEVEL 1 (
-  echo Ошибка: 2 тест не пройден
-    echo ----------------------
-) else (
-  echo 2 тест успешно пройден
-  echo ----------------------
-  set /a num+=1
-)
-
-  @echo 3 тест- как и второй(нужно доработать по-хорошему)
-echo the quick brown fox jumps over the lazy dog| java -jar app.jar > out.txt
-fc /a out.txt true_out.txt
-if ERRORLEVEL 1 (
-  echo Ошибка: 3 тест не пройден
-    echo ----------------------
-) else (
-  echo 3 тест успешно пройден
-    echo ----------------------
-  set /a num+=1
-)
-@echo Пройдено %num% тестов из 3-х
-
-
+@echo Passed %num% tests from 6
 
 pause

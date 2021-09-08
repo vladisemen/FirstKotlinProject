e87906311ad48ab0030dd9137eb5c1059bcf7898
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-09-08 11:02:14 +0700

    Update BUILD.bat

--- BUILD.bat
+++ BUILD.bat
@@ -1,1 +1,6 @@
-kotlinc app.kt -include-runtime -d app.jar
+md jar
+for /l %%i in (1,1,6) do kotlinc src/task%%i.kt src/functions.kt -include-runtime -d jar/task%%i.jar
+
+::It doesn't work
+CALL kotlinc clean install
+pause

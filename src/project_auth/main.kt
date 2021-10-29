b687860aa4abbbe173880b78e3314c5a94ec6875
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-10-29 16:08:07 +0700

    1.1.5

--- src/project_auth/main.kt
+++ src/project_auth/main.kt
@@ -4,3 +4,17 @@ val Users: List<User> = listOf(
     User("admin", "123"),
     User("user1", "qwerty123")
 )
+
+fun main(args: Array<String>) {
+    val list: Array<String> = if (args.isEmpty()) {
+        readLine().toString().split(" ").toTypedArray() //read stdin
+    } else {
+        args
+    }
+    if (list.size > 1){
+        val inputLogin = list[0]
+        val inputPass = list[1]
+        println(inputLogin)
+        println(inputPass)
+    }
+}

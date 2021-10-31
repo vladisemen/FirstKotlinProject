b990a66cf6b5ebe651c2e71db8385e07677505d9
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-10-31 15:03:23 +0700

    R1.2

--- src/project_auth/main.kt
+++ src/project_auth/main.kt
@@ -5,8 +5,8 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 val Users: List<User> = listOf(
-    User("admin", "202cb962ac59075b964b07152d234b70", "Salt"), // 123
-    User("user1", "3fc0a7acf087f549ac2b266baf94b8b1", "Salt")   // qwerty123
+    User("admin", "35d0239415e2371ee283a773f215c036", "Salt"), // 123
+    User("user1", "b614d9bc7599d324e730dafbee318881", "Salt1")   // qwerty123
 )
 
 fun main(args: Array<String>) {
@@ -25,12 +25,19 @@ fun main(args: Array<String>) {
         return
     }
 
-    if (inputUser.pass == getHash(inputPass)) {
+    if (inputUser.pass == getPassHashAndSolt(inputPass, inputUser.salt)) {
         print("Аутнетификация успешна")
     } else {
         print("Аутентификация неуспешна")
     }
 
+}
+
+/**
+ * Вернет хэшированный пароль (с добавлением соли)
+ */
+fun getPassHashAndSolt(pass: String, salt: String): String {
+    return getHash(getHash(pass) + salt)
 }
 
 /**

89c79f08a4144e66624bb037123b57e14f71ef8a
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-10-31 19:03:19 +0700

    1.3, 1.6

--- src/project_auth/main.kt
+++ src/project_auth/main.kt
@@ -1,5 +1,6 @@
 package project_auth
 
+import project_auth.models.Roles
 import java.math.BigInteger
 import java.security.MessageDigest
 
@@ -11,19 +12,26 @@ fun main(args: Array<String>) {
     }
     val inputLogin = list[0]
     val inputPass = list[1]
+    val inputRoleStr = list[2]
+    val inputRes = list[3]
     println(inputLogin)
     println(inputPass)
     val userDB = DateBase()
     val inputUser = userDB.findUserByLogin(inputLogin)
     if (inputUser == null) {
-        print("Пользователь не найден!")
+        println("Пользователь не найден!")
         return
     }
-
-    if (inputUser.pass == getPassHashAndSolt(inputPass, inputUser.salt)) {
-        print("Аутнетификация успешна")
+    val inputRole = roleStringToEnum(inputRoleStr)
+    if (inputUser.pass == getPassHashAndSolt(inputPass, inputUser.salt) && inputRole !== null ) {
+        println("Аутнетификация успешна")
+        if(userDB.checkResourceAccess(inputRes, inputRole, inputLogin)){
+            println("Есть Доступ!")
+        }else{
+            println("Нету доступа!")
+        }
     } else {
-        print("Аутентификация неуспешна")
+        println("Аутентификация неуспешна")
     }
 
 }
@@ -41,4 +49,23 @@ fun getPassHashAndSolt(pass: String, salt: String): String {
 fun getHash(password: String): String {
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(password.toByteArray())).toString(16).padStart(32, '0')
+}
+/**
+ * Вернет роль типа Enum если соотв, иначе вернет null
+ */
+fun roleStringToEnum(roleString: String): Roles? {
+    return when (roleString) {
+        "READ" -> {
+            Roles.READ
+        }
+        "WRITE" -> {
+            Roles.WRITE
+        }
+        "EXECUTE" -> {
+            Roles.EXECUTE
+        }
+        else -> {
+            null
+        }
+    }
 }

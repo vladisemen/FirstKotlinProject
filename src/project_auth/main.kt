13aaf212f05767fec15a4e8f5251302e681da66c
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-11-01 17:39:01 +0700

    Revert "R1.8 Добавлено разбиение текста на параметы"
    This reverts commit 8df7fc04ea0620691fa0bcce31a0efdb4a258922.

--- src/project_auth/main.kt
+++ src/project_auth/main.kt
@@ -1,44 +1,26 @@
 package project_auth
 
-import project_auth.models.RoleResource
-import project_auth.models.User
 import java.math.BigInteger
 import java.security.MessageDigest
 
 fun main(args: Array<String>) {
-    val text = if (args.isEmpty()) {
-        readLine().toString()
+    val list: Array<String> = if (args.isEmpty()) {
+        readLine().toString().split(" ").toTypedArray() //read stdin
     } else {
-        args.toString()
+        args
     }
-
-    val collectionParameter: MutableMap<String, String> = mutableMapOf()
-
-    for (parameterAndValue in text.split("-")) {
-        if (parameterAndValue != ""){
-            val parameter = parameterAndValue.toString().split(" ")
-            collectionParameter.put(
-                parameter[0],
-                parameter[1]
-            )
-        }
-    }
-
-    val dataUser = User(
-        collectionParameter.getValue("login"),
-        collectionParameter.getValue("pass")
-    )
-
-    println(dataUser.login)
-    println(dataUser.pass)
+    val inputLogin = list[0]
+    val inputPass = list[1]
+    println(inputLogin)
+    println(inputPass)
     val userDB = DateBase()
-    val inputUser = userDB.findUserByLogin(dataUser.login)
+    val inputUser = userDB.findUserByLogin(inputLogin)
     if (inputUser == null) {
         print("Пользователь не найден!")
         return
     }
 
-    if (inputUser.pass == getPassHashAndSolt(dataUser.pass, inputUser.salt)) {
+    if (inputUser.pass == getPassHashAndSolt(inputPass, inputUser.salt)) {
         print("Аутнетификация успешна")
     } else {
         print("Аутентификация неуспешна")
--- src/project_auth/models/User.kt
+++ src/project_auth/models/User.kt
@@ -1,8 +1,8 @@
 package project_auth.models
 
 data class User(
+    val id: Int,
     val login: String,
     val pass: String,
-    val id: Int = 0,
-    val salt: String = ""
+    val salt: String
 )

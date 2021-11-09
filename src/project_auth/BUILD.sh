1fbcf1f6fbcc1a8015d2ed6aa4872a7d37a677ec
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-11-09 18:24:31 +0700

    R1.12 Изменены вхожные значения TEST.sh и добавлены новые файлы в BUILD.sh

--- src/project_auth/BUILD.sh
+++ src/project_auth/BUILD.sh
@@ -1,1 +1,2 @@
-kotlinc main.kt -include-runtime -d main.jar
+kotlinc main.kt DateBase.kt models/Roles.kt models/RoleResource.kt models/User.kt -cp lib/kotlinx-cli-0.2.1.jar -include-runtime -d main.jar
+sleep 120s
--- src/project_auth/TEST.sh
+++ src/project_auth/TEST.sh
@@ -3,30 +3,30 @@ QuantitPassedTests=0
 declare -A CaseTest
 CaseTest[0]=""
 CaseTest[1]="-h"
-CaseTest[2]="-login user -pass zzz"
-CaseTest[3]="-pass zzz -login user"
-CaseTest[4]="-login !user! -pass zzz"
+CaseTest[2]="-login admin -pass 123"
+CaseTest[3]="-pass 123 -login admin"
+CaseTest[4]="-login !admin! -pass 123"
 CaseTest[5]="-login asd -pass dsa"
 CaseTest[6]="-login admin -pass 0000"
-CaseTest[7]="-login user -pass zzz -role READ -res A"
-CaseTest[8]="-login user -pass zzz -role qwe -res A"
+CaseTest[7]="-login admin -pass 123 -role READ -res A"
+CaseTest[8]="-login admin -pass 123 -role qwe -res A"
 CaseTest[9]="-login q -pass ytrewq -role EXECUTE -res A"
-CaseTest[10]="-login admin -pass 0000 -role READ -res A.B"
-CaseTest[11]="-login admin -pass 0000 -role WRITE -res A.B.C"
-CaseTest[12]="-login user -pass zzz -role qwe -res A.B.C"
-CaseTest[13]="-login user -pass zzz -role WRITE -res A.B"
-CaseTest[14]="-login user -pass zzz -role READ -res A.B -ds 2020-01-11 -de 2020-01-12 -vol 10"
-CaseTest[15]="-login user -pass zzz -role READ -res A.B -ds 2020.01.11 -de 2020.01.12 -vol 10"
-CaseTest[16]="-login user -pass zzz -role READ -res A.B -ds 2020-01-1 -de 2020-01-60 -vol 10"
-CaseTest[17]="-login user -pass zzz -role READ -res A.B -ds 2020-01-11 -de 2020-10-12 -vol hgh"
-CaseTest[18]="-login user -pass zzz -role WRITE -res A -ds 2020-01-12 -de 2020-01-13 -vol 10"
+CaseTest[10]="-login admin -pass 123 -role READ -res A.B"
+CaseTest[11]="-login admin -pass 123 -role WRITE -res A.B.C"
+CaseTest[12]="-login admin -pass 123 -role qwe -res A.B.C"
+CaseTest[13]="-login admin -pass 123 -role WRITE -res A.B"
+CaseTest[14]="-login admin -pass 123 -role READ -res A.B -ds 2020-01-11 -de 2020-01-12 -vol 10"
+CaseTest[15]="-login admin -pass 123 -role READ -res A.B -ds 2020.01.11 -de 2020.01.12 -vol 10"
+CaseTest[16]="-login admin -pass 123 -role READ -res A.B -ds 2020-01-1 -de 2020-01-60 -vol 10"
+CaseTest[17]="-login admin -pass 123 -role READ -res A.B -ds 2020-01-11 -de 2020-10-12 -vol hgh"
+CaseTest[18]="-login admin -pass 123 -role WRITE -res A -ds 2020-01-12 -de 2020-01-13 -vol 10"
 
-expectedExitCodes=(1 1 0 0 2 3 0 0 5 3 0 0 5 6 0 7 7 7 6)
+expectedExitCodes=(1 0 0 0 3 3 4 0 5 3 0 0 5 0 0 1 1 1 0)
 
 for ((i = 0; i < "$QuantityTest"; i++)); do
   test=${CaseTest[$i]}
   expectedExitCode=${expectedExitCodes[$i]}
-  java -jar "main.jar" ""${test}""
+  java -cp lib/kotlinx-cli-0.2.1.jar:main.jar project_auth.MainKt ""${test}"" 
   exitCode="$?"
   if [ "$exitCode" == "$expectedExitCode" ]; then
     echo "CaseTest $i passed exit code $exitCode"

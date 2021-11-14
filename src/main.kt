0485b31afdc098b8c857abbcf6761077469304a8
vladisemen <40513696+vladisemen@users.noreply.github.com>
2021-11-15 00:25:20 +0700

    #6 7

--- src/main.kt
+++ src/main.kt
@@ -119,9 +119,9 @@ fun isAAAFun(
 fun isDateAndValueValid(ds: String, de: String, value: String): Boolean {
     val datePattern = "[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
 
-    if (Regex(datePattern).find(ds) == null
-        || Regex(datePattern).find(de) == null
-        || Regex("\\d+").find(value) == null
+    if (Regex(datePattern).matches(ds)
+        || Regex(datePattern).matches(de)
+        || Regex("\\d+").matches(value)
     ) {
         return true
     }

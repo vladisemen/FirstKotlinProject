d0f1a2600ec2ac90bafc6a4ef9a7bae3d6e74578
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-11-21 15:00:11 +0700

    #7 №11 Исправлен ExitCodeEnum

--- src/models/ExitCodeEnum.kt
+++ src/models/ExitCodeEnum.kt
@@ -1,54 +1,20 @@
 package models
 
-enum class ExitCodeEnum {
-    ZERO {
-        override fun printExitCode(): Int {
-            println("успех")
-            return 0
-        }
-    },
-    FIRST { // Парсер не дает выводить
-        override fun printExitCode(): Int {
-            println("вывод справки")
-            return 1
-        }
-    },
-    SECOND {
-        override fun printExitCode(): Int {
-            println("неверный формат логина")
-            return 2
-        }
-    },
-    THREE {
-        override fun printExitCode(): Int {
-            println("неверный пароль")
-            return 3
-        }
-    },
-    FOUR {
-        override fun printExitCode(): Int {
-            println("неизвестная роль")
-            return 4
-        }
-    },
-    FIVE {
-        override fun printExitCode(): Int {
-            println("нет доступа")
-            return 5
-        }
-    },
-    SIX {
-        override fun printExitCode(): Int {
-            println("вывод справки")
-            return 6
-        }
-    },
-    SEVEN {
-        override fun printExitCode(): Int {
-            println("некорректная активность")
-            return 7
-        }
-    };
-
-    abstract fun printExitCode(): Int
+enum class ExitCodeEnum(val code: Int) {
+    ZERO(0),
+    FIRST(1),
+    SECOND(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7)
 }
+//успех
+//вывод справки
+//неверный формат логина
+//неверный пароль
+//неизвестная роль
+//нет доступа
+//вывод справки
+//некорректная активность

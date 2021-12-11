e4adbd08fd95b2d131c7b3609025a85fc88432eb
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-12-11 23:58:36 +0700

    RL4. Добавлен макет

--- src/research_work/log/main.kt
+++ src/research_work/log/main.kt
@@ -1,5 +1,6 @@
 package research_work.log
 
+import org.apache.log4j.Level
 import org.apache.log4j.Logger
 
 fun main() {
@@ -11,11 +12,10 @@ class Log() {
     private val log: Logger = Logger.getLogger(Log::class.java)
 
     fun log() {
-        log.trace("Trace Message!");
-        log.debug("Debug Message!");
-        log.info("Info Message!");
-        log.warn("Warn Message!");
-        log.error("Error Message!");
-        log.fatal("Fatal Message!");
+        log.info("Info Message!")
+
+        log.log(Level.FATAL, "Logger Name :: " + Log::class.java + " :: Passed Message ::")
+        log.log(Level.ERROR, "Logger Name :: " + Log::class.java + " :: Not Passed Message ::")
+        log.getParent().log(Level.ERROR, "Root Logger :: Passed Message As Root Is Configured For ERROR Level messages")
     }
 }

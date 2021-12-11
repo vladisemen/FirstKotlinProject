7d6f4ba184d9594af1e58a7a72b0051d3fc82596
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-12-11 20:08:20 +0700

    RB4. выполнение DML/CRUD запросов

--- src/research_work/db/main.kt
+++ src/research_work/db/main.kt
@@ -5,6 +5,7 @@ import java.sql.*
 fun main() {
     val conn: Connection = connection()
 
+    ddl(conn);
     dml(conn);
 }
 
@@ -20,8 +21,28 @@ fun connection(): Connection {
     }
 }
 
+fun ddl(con: Connection) {
+    val st = con.createStatement()
+    st.executeUpdate("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255))")
+    st.executeUpdate("DROP TABLE TEST")
+}
+
 fun dml(con: Connection) {
     val st = con.createStatement()
     st.executeUpdate("CREATE TABLE TEST(ID INT PRIMARY KEY, NAME VARCHAR(255))")
+
+    st.executeUpdate("INSERT INTO TEST VALUES(1, 'World')")
+    st.executeUpdate("UPDATE TEST SET NAME='Hi' WHERE ID=1")
+    val select = st.executeQuery("SELECT * FROM TEST")
+
+    while (select.next()) {
+        val id: Int = select.getInt("ID")
+        val name: String = select.getString("NAME")
+
+        print("ID: $id")
+        println(", name: $name")
+    }
+
+    st.executeUpdate("DELETE FROM TEST WHERE ID=1")
     st.executeUpdate("DROP TABLE TEST")
 }

11d3042900e2ae1a3e586348a8cf2baa7b230414
demon777veka2 <58589191+demon777veka2@users.noreply.github.com>
2021-12-18 19:20:46 +0700

    R4.3. Добавлена проверку на существования схемы БД

--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,16 @@
 import services.Authentication
 import services.Authorization
 import services.Parser
+import services.flyway.Flyway
 import services.log.Log
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
-
+    val flyway = Flyway()
     val parser = Parser(args)
     val objectParams = parser.parser()
 
+    flyway.startFirstMigration()
     val authentication = Authentication(objectParams)
     val authorization = Authorization(objectParams)
 
